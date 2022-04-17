76c1e562789835990dddb29e8f94edfb40da902f
eugen1701 <eugen.flocea@yahoo.com>
2022-04-18 00:25:01 +0300

    After adding bootstrap

--- src/components/auth/login/Login.tsx
+++ src/components/auth/login/Login.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./Login.scss";
-import Frorm from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 
 export interface AuthInterface {
   authenticationError: Error | null;
@@ -38,54 +38,32 @@ export const Login: React.FC = () => {
   const { email, password } = state;
 
   function handleLogin(e: { preventDefault: () => void }) {}
-
+//color-overlay d-flex justify-content-center align-items-center
   return (
     <div id="login">
-      <h1 id="login__heading" className="text-black text-large">
-        Login page
-      </h1>
-      <br />
-      <form id="login__form">
-        <ul id="login__form-page" className="ul login__form-ul">
-          <li className="input">
-            <label htmlFor="user-username">Email</label>
-            <input
-              type="email"
-              name="username"
-              id="login__username"
-              value={email}
-              onChange={(event) =>
-                setState({ ...state, email: event.target.value })
-              }
-            />
-          </li>
-          <li className="input">
-            <label htmlFor="user-password">Password</label>
-            <input
-              type="password"
-              name="user-password"
-              id="login__user-password"
-              value={password}
-              onChange={(event) =>
-                setState({ ...state, password: event.target.value })
-              }
-            />
-          </li>
-        </ul>
-        <div className="checkbox mb-3">
-          <label>
-            <input type="checkbox" value="remember-me" /> Remember me
-          </label>
-        </div>
-        <div id="login__form-action" className="flex-row-center-y">
-          <button
-            className="w-100 btn btn-lg btn-primary"
-            onClick={handleLogin}
-          >
-            Login
-          </button>
-        </div>
-      </form>
+      <div className="p-5 m-auto rounded-lg">
+        <h1 id="login__heading" className="text-black text-large">
+          Login page
+        </h1>
+        <Form className="rounded p-4 p-sm-3">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control type="email" placeholder="Email"/>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password"/>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Remember Me"/>
+          </Form.Group>
+
+          <Button variant="primary btn-block w-100" type="submit" id="login-button">Login</Button>
+
+        </Form>
+      </div>
     </div>
   );
 };
