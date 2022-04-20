de346b9a4c05c574fe3ad0af08253341cad53de8
eugen1701 <eugen.flocea@yahoo.com>
2022-04-21 00:31:36 +0300

    Change logo

--- src/components/auth/login/Login.tsx
+++ src/components/auth/login/Login.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./Login.scss";
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Card } from "react-bootstrap";
 
 export interface AuthInterface {
   authenticationError: Error | null;
@@ -38,32 +38,38 @@ export const Login: React.FC = () => {
   const { email, password } = state;
 
   function handleLogin(e: { preventDefault: () => void }) {}
-//color-overlay d-flex justify-content-center align-items-center
+
+  //color-overlay d-flex justify-content-center align-items-center
   return (
-    <div id="login">
-      <div className="p-5 m-auto rounded-lg">
-        <h1 id="login__heading" className="text-black text-large">
-          Login page
-        </h1>
-        <Form className="rounded p-4 p-sm-3">
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email Address</Form.Label>
-            <Form.Control type="email" placeholder="Email"/>
-          </Form.Group>
+      <Card className="border-5 shadow bg-light" id="card">
+    <div className="p-5 m-auto rounded-lg">
+      <h1 id="login__heading" className="text-black text-large">
+        Login page
+      </h1>
+      <Form className="rounded p-4 p-sm-3">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control type="email" placeholder="Email" />
+        </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password"/>
-          </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Remember Me"/>
-          </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Remember Me" />
+        </Form.Group>
 
-          <Button variant="primary btn-block w-100" type="submit" id="login-button">Login</Button>
-
-        </Form>
-      </div>
+        <Button
+          variant="primary btn-block w-100"
+          type="submit"
+          id="login-button"
+        >
+          Login
+        </Button>
+      </Form>
     </div>
+      </Card>
   );
 };
