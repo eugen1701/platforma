5b2ef21d8e0172d8b00c95d1585863ba4ccf348c
eugen1701 <eugen.flocea@yahoo.com>
2022-04-26 16:00:09 +0300

    Added grid format and Logo on Login page

--- src/components/auth/login/Login.tsx
+++ src/components/auth/login/Login.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./Login.scss";
 import { Form, Button, Card } from "react-bootstrap";
+import { Logo } from "../../logo/Logo";
 
 export interface AuthInterface {
   authenticationError: Error | null;
@@ -41,35 +42,42 @@ export const Login: React.FC = () => {
 
   //color-overlay d-flex justify-content-center align-items-center
   return (
-      <Card className="border-5 shadow bg-light" id="card">
-    <div className="p-5 m-auto rounded-lg">
-      <h1 id="login__heading" className="text-black text-large">
-        Login page
-      </h1>
-      <Form className="rounded p-4 p-sm-3">
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email Address</Form.Label>
-          <Form.Control type="email" placeholder="Email" />
-        </Form.Group>
+    <div className="container-fluid d-flex">
+      <div className="row w-100">
+        <div id="in-page-logo" className="col">
+          {<Logo />}
+        </div>
+        <Card className="col-xxl-auto border-5 shadow bg-light" id="card">
+          <div className="p-5 m-auto rounded-lg">
+            <h1 id="login__heading" className="text-black text-large">
+              Login page
+            </h1>
+            <Form className="rounded p-4 p-sm-3">
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control type="email" placeholder="Email" />
+              </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Remember Me" />
-        </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Remember Me" />
+              </Form.Group>
 
-        <Button
-          variant="primary btn-block w-100"
-          type="submit"
-          id="login-button"
-        >
-          Login
-        </Button>
-      </Form>
+              <Button
+                variant="primary btn-block w-100"
+                type="submit"
+                id="login-button"
+              >
+                Login
+              </Button>
+            </Form>
+          </div>
+        </Card>
+      </div>
     </div>
-      </Card>
   );
 };
