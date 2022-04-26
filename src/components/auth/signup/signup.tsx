3a2350a9f038136efd8659889f5828611f343376
eugen1701 <eugen.flocea@yahoo.com>
2022-04-26 16:11:43 +0300

    Added grid format and Logo on Signup page

--- src/components/auth/signup/signup.tsx
+++ src/components/auth/signup/signup.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import {Button, Card, Form} from "react-bootstrap";
+import {Logo} from "../../logo/Logo";
 
 export const Signup: React.FC = () => {
 
@@ -8,10 +9,16 @@ export const Signup: React.FC = () => {
 
     }
   return (
+<div className="container-fluid d-flex">
+    <div className="row w-100   ">
+    <div className="col" id="in-page-logo">
+        {<Logo/>}
+    </div>
+        <div className="col">
     <Card className="border-5 shadow bg-light" id="card">
         <div className="p-5 m-auto rounded-lg">
             <h1 id="signup__heading" className="text-black text-large">
-                Sign up page
+                Sign up
             </h1>
             <Form className="rounded p-4 p-sm-3">
                 <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -37,8 +44,11 @@ export const Signup: React.FC = () => {
                     Sign up
                 </Button>
             </Form>
-            <h5>From here the user should be redirected to a page where he can fill in the personal information in order to make the recomandation possible</h5>
+            <h5 >From here the user should be redirected to a page where he can fill in the personal information in order to make the recomandation possible</h5>
         </div>
     </Card>
+        </div>
+    </div>
+</div>
   );
 };
