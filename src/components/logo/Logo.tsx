bfb1972687c30bbf3ca535e51a300250a6e81d0b
eugen1701 <eugen.flocea@yahoo.com>
2022-04-26 15:02:41 +0300

    Changed logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -3,7 +3,6 @@ import "./Logo.scss"
 
 export const Logo:React.FC = () => {
     return <div id="logo">
-        <h1 id="word-in">In</h1>
-        <h1 id="word-work">Work</h1>
+        <h1 id="word-in">In<span id="word-work">Work</span></h1>
     </div>
 }
